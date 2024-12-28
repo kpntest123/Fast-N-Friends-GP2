7b0f80ca25e39196414b915b93a731e2a4be624f
Martiny <118130091+kpntest123@users.noreply.github.com>
2024-12-28 16:59:12 +0100

    Yep

---
+++ legal-notice.php
@@ -0,0 +1,26 @@
+<?php
+/* Template Name: Page de mentions légales */
+get_header();
+?>
+
+<section class="mentions-legales">
+<div class="container mt-5">
+    <h1 class="text-center">Mentions légales</h1>
+
+    <div class="row">
+      <div class="col-12">
+        <p><strong>Nom de la société​ :</strong> Fast 'N Friends</p>
+        <p><strong>Forme juridique :</strong> SPRL</p>
+        <p><strong>Adresse postale du siège social​ :</strong> Rue de la Poste 111, 1030 Schaerbeek</p>
+        <p><strong>Téléphone et E-Mail​ :</strong> 0451 42 52 85 / <a href="mailto:fastnfriends@gmail.com">fastnfriends@gmail.com</a></p>
+        <p><strong>Numéro BCE :</strong> 0393.911.13</p>
+        <p><strong>TVA :</strong> BE0384373683</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php
+get_footer();
+?>
